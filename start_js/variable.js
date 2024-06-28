de786c6c994e6a5336fff170fd83be56350a56c0
ashu6879 <144018820+ashu6879@users.noreply.github.com>
2024-06-28 10:34:38 +0000

    video till 7th

---
+++ start_js/variable.js
@@ -0,0 +1,9 @@
+/* documentation for this js=mdn&tc39 */
+const thisConst = 1000
+let thiLet = "hii i am let"
+var thisVar = "hey var here"
+
+let thisUndefined;
+
+console.log(thisConst)
+console.table([thisConst,thisUndefined,thisVar])
